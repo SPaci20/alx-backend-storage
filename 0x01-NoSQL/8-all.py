39b5d07522ae6069afdd0ae624aaff9ba409e2b8
SPaci20 <spaci20@gmail.com>
2024-03-26 00:28:58 -0700

    Task-8

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,16 @@
+sk 8's module.
+'''
+
+
+def list_all(mongo_collection):
+        '''Lists all documents in a collection.
+            '''
+                return [doc for doc in mongo_collection.find()]#!/usr/bin/env python3
+            '''Task 8's module.
+            '''
+
+
+            def list_all(mongo_collection):
+                '''Lists all documents in a collection.
+                    '''
+                        return [doc for doc in mongo_collection.find()]
